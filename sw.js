7106cb3def5a52521f0e055c158182e81c67dd72
ZHTing <Ayu1948@163.com>
2020-08-19 10:05:46 +0800

    测试pwa

--- sw.js
+++ sw.js
@@ -1,42 +1,25 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-var cacheStorageKey = 'minimal-pwa-1'
-var cacheList=[
-  '/',
-  'index.html',
-  'main.css',
-  'testgame.jpg'
-]
-self.addEventListener('install',e =>{
-  e.waitUntil(
-    caches.open(cacheStorageKey)
-    .then(cache => cache.addAll(cacheList))
-    .then(() => self.skipWaiting())
-  )
-})
-self.addEventListener('fetch',function(e){
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('fox-store').then(function(cache) {
+     return cache.addAll([
+       '/testgame/',
+       '/testgame/index.html',
+       '/testgame/index.js',
+       '/testgame/style.css',
+       '/testgame/images/fox1.jpg',
+       '/testgame/images/fox2.jpg',
+       '/testgame/images/fox3.jpg',
+       '/testgame/images/fox4.jpg'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
   e.respondWith(
-    caches.match(e.request).then(function(response){
-      if(response != null){
-        return response
-      }
-      return fetch(e.request.url)
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
-  )
-})
-self.addEventListener('activate',function(e){
-  e.waitUntil(
-    //获取所有cache名称
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        // 获取所有不同于当前版本名称cache下的内容
-        cacheNames.filter(cacheNames => {
-          return cacheNames !== cacheStorageKey
-        }).map(cacheNames => {
-          return caches.delete(cacheNames)
-        })
-      )
-    }).then(() => {
-      return self.clients.claim()
-    })
-  )
-})
+  );
+});
